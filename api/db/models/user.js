99bc8f2a3620d5784eee4879f9abe09f69bdc055
Abdulfatai Aka <abdulfataiaka@gmail.com>
2021-12-26 21:42:19 +0100

    check db on startup

--- api/db/models/user.js
+++ api/db/models/user.js
@@ -18,8 +18,8 @@ module.exports = (sequelize, DataTypes) => {
     id: {
       allowNull: false,
       primaryKey: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
     },
     username: {
       allowNull: false,
